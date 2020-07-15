27daeb9e8f72acc2cbaa6052a4ec7ac296b842c5
Khrystyna Pylypiv <pylypiv.kris.iv@ukr.net>
2020-07-15 17:48:40 +0300

    HW 1

---
+++ HW 1/js.js
@@ -0,0 +1,57 @@
+//Task 2
+//console.log("Pylypiv");
+
+//Task 3
+let color1 = "red";
+let color2 = "blue";
+alert(color1);
+alert(color2);
+color1 = color2;
+alert(color1);
+alert(color2);
+
+//Task 4
+let obj = {
+	day: "Wednesday",
+	date: 15,
+	wordkingDay: true,
+	holiday: undefined,
+	dayOff: null
+}
+
+//Task 5
+let isAdult = confirm("Are you an adult?");
+console.log(isAdult);
+
+//Task 6;
+let myName = "Khrystyna";
+let mySyrname = "Pylypiv";
+let group = "Javascropt Core";
+let yearOfBirth = 1996;
+let isMarried = false;
+//Numbers
+console.log(yearOfBirth);
+//Boolean
+console.log(isMarried);
+//String
+console.log(myName);
+console.log(mySyrname);
+console.log(group);
+let dayOfBirth;
+let favouriteBook = null;
+console.log(typeof dayOfBirth);
+console.log(typeof favouriteBook);
+
+//Task 7
+let login = prompt("What is your login?");
+let email = prompt("What is your email?");
+let password = prompt("What is your password?");
+alert("Dear " + login + " , your email is " + email + ", your password is " + password);
+
+//Task 8
+let secondsInHour = 60 * 60;
+let secondsInDay = 24 * secondsInHour;
+let secondsInMonth = 31 * secondsInDay;
+console.log(secondsInHour);
+console.log(secondsInDay);
+console.log(secondsInMonth);
