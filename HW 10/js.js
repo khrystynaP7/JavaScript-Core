499294406c8790c024ee3c36ef48b630fc343625
Khrystyna Pylypiv <pylypiv.kris.iv@ukr.net>
2020-08-17 17:12:50 +0300

    HW 10

--- HW 10/js.js
+++ HW 10/js.js
@@ -17,4 +17,53 @@ console.log(name4); // "Bob"
 console.log(age4); // 26
 
 //Task 3
+function mul() {
+    let args = [...arguments];
+    let result = 0;
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] === 'number') {
+            result+= args[i];
+        }
+    }
+    return result;
+}
+console.log(mul(1, "str", 2, 3, true)); // 6
+
+//Task 4
+let server = {
+   data: 0,
+   convertToString(callback) {
+      callback(() => `${this.data}`);
+   }
+};
+let client = {
+   server,
+   result: "",
+   calc(data) {
+      this.server.data = data;
+      this.server.convertToString(this.notification());
+   },
+   notification() {
+      return callback => {
+         this.result = callback();
+      };
+   }
+};
+client.calc(123);
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
+
+//Task 5
+function mapBuilder(arr1, arr2) {
+    const newMap = new Map();
+    for (let i = 0; i < arr1.length; i++) {
+        newMap.set(arr1[i], arr2[i])
+    }
+    return newMap
+}
+
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+let map = mapBuilder(keys, values);
+console.log(map.size);
 
